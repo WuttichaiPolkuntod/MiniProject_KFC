9a246f53a00fae50691612e0acd56f57f37b4f49
WuttichaiPolkuntod <65309010045@tatc.ac.th>
2022-09-20 10:43:34 +0700

    fix connect

--- connect.php
+++ connect.php
@@ -2,7 +2,7 @@
     $host="localhost";
     $user="root";
     $password="";
-    $dbname="MiniProject";
+    $dbname="miniproject_kfc";
 
     $con=mysqli_connect($host,$user,$password,$dbname) or die ("ไม่สามารถเชื่อมต่อ database ได้");
     $con->query("SET NAMES UTF8");
